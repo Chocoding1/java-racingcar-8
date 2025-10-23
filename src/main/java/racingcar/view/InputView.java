9815ba1f6716930081da65139209aa84614e79bd
chocoding <galmeagi2@naver.com>
2025-10-23 15:02:56 +0900

    Refactor(InputView): 메서드명 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -19,7 +19,7 @@ public class InputView {
     private static final String CAR_NAMES_INPUT_SCREEN = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String FREQUENCY_INPUT_SCREEN = "시도할 횟수는 몇 회인가요?";
 
-    public String getCarNames() {
+    public String getInitialCarNames() {
         System.out.println(CAR_NAMES_INPUT_SCREEN);
         return Console.readLine();
     }
