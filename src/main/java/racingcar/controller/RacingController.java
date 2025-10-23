897f04b37f8665a00a48e615e31ae78e4ced4e7d
chocoding <galmeagi2@naver.com>
2025-10-23 14:55:46 +0900

    Feat: 시도 횟수만큼 경주 진행 기능 구현

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,16 +1,50 @@
 package racingcar.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import racingcar.model.Car;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class RacingController {
 
+    private static RacingController instance;
+
     private final InputView inputView = InputView.getInstance();
+    private final OutputView outputView = OutputView.getInstance();
+
+    private RacingController() {
+    }
+
+    public static RacingController getInstance() {
+        if (instance == null) {
+            instance = new RacingController();
+        }
+        return instance;
+    }
 
     public void startRacing() {
         // 자동차 이름 입력받기
-        String carNames = inputView.getCarNames();
+        String initialCarNames = inputView.getInitialCarNames();
         // 횟수 입력받기
         int frequency = inputView.getFrequency();
-    }
 
+        // 자동차 이름 분리해서 리스트로 만들기
+        String[] carNames = initialCarNames.split(",");
+
+        // 각 리스트 돌면서 자동차 객체 생성하여 자동차 객체 리스트 만들기
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+
+        // 횟수 반복
+        for (int i = 0; i < frequency; i++) {
+            // 리스트 돌면서 자동차 전진
+            for (Car car : cars) {
+                car.move();
+            }
+        }
+    }
 }
---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,35 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car{
+
+    private final String name;
+    private int distance;
+
+    public Car(String name) {
+        validateNameLength(name);
+        this.name = name;
+        this.distance = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void move() {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            distance++;
+        }
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() >= 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
+    }
+}
