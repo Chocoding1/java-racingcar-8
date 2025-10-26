8d1d50306d6f6a025a8f2a1286e0ed782cb91281
Chocoding1 <galmeagi2@naver.com>
2025-10-27 00:12:06 +0900

    Test(ParticipatingCarsTest): 단위 테스트 작성

---
+++ src/test/java/racingcar/model/ParticipatingCarsTest.java
@@ -0,0 +1,20 @@
+package racingcar.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ParticipatingCarsTest {
+
+    @Test
+    @DisplayName("참가 자동차 수가 2대 미만일 시 IllegalArgumentException 예외 발생")
+    void throw_exception_when_number_of_cars_less_than_2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> new ParticipatingCars("cho"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+}
