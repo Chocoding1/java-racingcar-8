174130b24baa1cb757cd8eb4a90dd705291951b6
Chocoding1 <galmeagi2@naver.com>
2025-10-27 16:13:22 +0900

    Test(CarTest): 단위 테스트 추가

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,8 +1,10 @@
 package racingcar.model;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -38,5 +40,37 @@ class CarTest {
             Car car = new Car(carName);
             assertThat(car.getDistance()).isEqualTo(0);
         });
+    }
+
+    @Test
+    @DisplayName("랜덤 숫자가 기준 숫자(4) 이상일 시 앞으로 전진")
+    void move_forward_when_randomNumber_bigger_than_conditionNUmber() {
+        //given
+        Car car = new Car("cho");
+
+        //when & then
+        assertRandomNumberInRangeTest(
+                () -> {
+                    car.move();
+                    assertEquals(1, car.getDistance());
+                },
+                4
+        );
+    }
+
+    @Test
+    @DisplayName("랜덤 숫자가 기준 숫자(4) 미만 시 정지")
+    void stop_when_randomNumber_bigger_than_conditionNUmber() {
+        //given
+        Car car = new Car("cho");
+
+        //when & then
+        assertRandomNumberInRangeTest(
+                () -> {
+                    car.move();
+                    assertEquals(0, car.getDistance());
+                },
+                3
+        );
     }
 }
